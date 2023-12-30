0296f51b8bf8926b85df2303b0277c4ba7252fe2
pashmakhj <83176713+pashmakhj@users.noreply.github.com>
2023-12-30 15:24:48 +0330

    completing data models V1

--- back/back/settings.py
+++ back/back/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
+    'cart',
+    'dish',
+    'restaurant',
+    'address',
+    'rest_framework',
+    'phonenumber_field',
+
 ]
 
 MIDDLEWARE = [
@@ -118,6 +126,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+REST_FRAMEWORK={
+    'DEFAULT_PERMISSION_CLASSES': []
+    ,
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
